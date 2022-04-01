dd18f632cacd9cf077b0d70994dcc7864741498b
aktashka <aktan.imanaliev003@gmail.com>
2022-04-01 20:14:12 +0600

    lesson9

---
+++ src/components/item-list/item-list.js
@@ -0,0 +1,37 @@
+
+import React, { Component, useState, useEffect } from 'react';
+import SwapiService from '../../services/swapi-service';
+
+import './item-list.css';
+
+const ItemList = () => {
+  const [state, setState] = useState([]);
+  const {data} = state;
+
+  const swapi = new SwapiService();
+
+  useEffect(() => {
+    swapi.getAllPeople().then((data) => {
+      setState(data)
+    })
+  }, [])
+
+
+
+  const content = state.map(item => {
+    return (
+      <li id={item.id} className="list-group-item">
+        {item.name}
+      </li>
+    )
+  })
+
+  return (
+    <ul className="item-list list-group">
+      {content}
+    </ul>
+
+  );
+
+}
+export default ItemList
